6f730753ea5a2d4c30d67401f82f116e012e590e
Rahul <87407780+wreck2053@users.noreply.github.com>
2024-11-08 11:12:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 80;
 
 app.get('/health', (req, res) => {
   const isHealthy = process.env.HEALTH_STATUS === 'healthy'; // Use an environment variable to toggle
-  if (isHealthy) {
+  if (!isHealthy) {
     res.status(200).send('Healthy');
   } else {
     res.status(500).send('Unhealthy');
